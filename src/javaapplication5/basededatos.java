632ff5fe4b54c167174839aa964b09f438d5454f
SENA <SENA@MEDDFPCPD7F3011>
2017-05-05 08:22:42 -0500

    base de datos 1.0

--- src/javaapplication5/basededatos.java
+++ src/javaapplication5/basededatos.java
@@ -10,5 +10,38 @@ package javaapplication5;
  * @author SENA
  */
 public class basededatos {
+    private String tipochat;
+    private String Nickusuario;
+    private String Fotousuario;
+    private String Mensaje;
+    private String Fechamensaje;
     
+    public void settipochat(String tipochatP){
+     this.tipochat=tipochatP;   
+    }
+    public String gettipochat()
+    {
+        return tipochat;
+    }
+    public void setNickusuario(String NickusuarioP){
+     this.Nickusuario=NickusuarioP;   
+    }
+    public String getNickusuario()
+    {
+        return Nickusuario;
+    }
+    public void setFotousuario(String FotousuarioP){
+     this.Fotousuario=FotousuarioP;   
+    }
+    public String getFotousuario()
+    {
+        return Fotousuario;
+    }
+    public void setMensaje(String MensajeP){
+     this.Mensaje=MensajeP;   
+    }
+    public String getMensaje()
+    {
+        return Mensaje;
+    }
 }
